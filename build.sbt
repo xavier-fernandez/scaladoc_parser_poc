3379857734d281b6381a0770bc2f6e02c684cc88
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-02-24 09:10:49 +0100

    Global POC refactor

--- build.sbt
+++ build.sbt
@@ -6,5 +6,8 @@ scalaVersion := "2.11.8"
 
 resolvers += Resolver.bintrayIvyRepo("scalameta", "maven")
 
-libraryDependencies += "org.scalameta" %% "scalameta" % "1.6.0-671"
-libraryDependencies += "org.scalameta" %% "contrib" % "1.6.0-671"
+libraryDependencies += "org.scalameta" %% "scalameta" % "1.6.0"
+libraryDependencies += "org.scalameta" %% "contrib" % "1.6.0"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.4" % "test"
---
+++ src/test/scala/ScaladocParserTest.scala
@@ -0,0 +1,154 @@
+
+import org.scalatest.FunSuite
+
+import scala.meta.contrib._
+import scala.meta.{Defn, _}
+import DocToken._
+import scala.meta.tokens.Token.Comment
+
+/**
+  * Test for [[ScaladocParser]]
+  */
+class ScaladocParserTest extends FunSuite {
+
+  def parseString(commentCode: String): Seq[DocToken] = {
+    val code = commentCode.parse[Source].get
+    val comments = AssociatedComments(code.tokens)
+    val defnClass = code.collectFirst { case t: Defn.Class => t }.get
+    val comment: Comment = comments.leading(defnClass).head
+    ScaladocParser.parseScaladoc(comment)
+  }
+
+  test("example usage") {
+    assert(
+      parseString(
+        """
+          | /** Example scaladoc **/
+          | case class foo(bar: String)
+        """.stripMargin
+      ).toString() === "List(Description(Example scaladoc))"
+    )
+  }
+
+  test("indentation checks") {
+
+    val expectedBody: String = "BODY"
+    val expectedResult: Seq[DocToken] = Seq(DocToken(Description, expectedBody))
+
+    assert(
+      parseString(
+        s"""
+         /** $expectedBody*/
+          case class foo(bar: String)
+         """
+      ) === expectedResult
+    )
+    assert(
+      parseString(
+        s"""
+         /** $expectedBody
+          */
+          case class foo(bar: String)
+         """
+      ) === expectedResult
+    )
+    assert(
+      parseString(
+        s"""
+         /**       $expectedBody
+          */
+          case class foo(bar: String)
+         """
+      ) === expectedResult
+    )
+    assert(
+      parseString(
+        s"""
+         /**
+          *$expectedBody
+          */
+          case class foo(bar: String)
+         """
+      ) === expectedResult
+    )
+    assert(
+      parseString(
+        s"""
+         /**
+          *
+          *
+          *
+          *$expectedBody
+          */
+          case class foo(bar: String)
+         """
+      ) === expectedResult
+    )
+    assert(
+      parseString(
+        s"""
+         /**
+          *
+            *
+        *  $expectedBody
+         *
+          */
+          case class foo(bar: String)
+         """.stripMargin
+      ) === expectedResult
+    )
+  }
+
+  // TODO: Add the rest of the labels
+  test("label parsing") {
+
+    // DocText
+    val textPrefix = "Body"
+
+    // DocConstructor
+    val constructorBody = s"$textPrefix-Constructor"
+
+    // DocParam
+    val paramName = "parameterName"
+    val paramBody = s"parameterBody"
+
+    // DocTypeParam
+    val tparamName = "T"
+    val tparamBody = "Simple parameter"
+
+    // DocThrows
+    val throwsName = "IllegalArgumentException"
+    val throwsBody = "when something fails"
+
+    // DocSee
+    val seeBody = "See body"
+
+    // Expectations
+    val expectedStructure = Seq(
+      DocToken(Description, textPrefix),
+      DocToken(Constructor, constructorBody),
+      DocToken(Param, paramName, paramBody),
+      DocToken(TypeParam, tparamName, tparamBody),
+      DocToken(Throws, throwsName, throwsBody),
+      DocToken(See, seeBody)
+    )
+
+    assert(
+      parseString(
+        s"""
+        /**
+         * $textPrefix
+         * ${Constructor.label} $constructorBody
+         * ${Param.label} $paramName $paramBody
+         * ${TypeParam.label} $tparamName $tparamBody
+         * ${Throws.label} $throwsName $throwsBody
+         * ${See.label} $seeBody
+         */
+         case class foo(bar: String)
+         """
+      ) === expectedStructure
+    )
+  }
+
+  // TODO: Test for multine token merging
+}
